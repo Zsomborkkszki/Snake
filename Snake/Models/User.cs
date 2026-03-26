07f9bad737373840b287cf883827feae20382b94
Zsombor <szilagyizs@kkszki.hu>
2026-03-26 12:02:58 +0100

    User class létrehozása

---
+++ Snake/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake.Models
+{
+    internal class User
+    {
+        int id;
+        string nev;
+        int pontszam;
+
+        public User(int id, string nev, int pontszam)
+        {
+            this.Id = id;
+            this.Nev = nev;
+            this.Pontszam = pontszam;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Nev { get => nev; set => nev = value; }
+        public int Pontszam { get => pontszam; set => pontszam = value; }
+    }
+}
